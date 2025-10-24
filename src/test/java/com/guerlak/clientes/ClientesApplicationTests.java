ae3722cd6927637ae5bc901bec0a82423a62dc36
Rafael Leite <guerlak@MacBook-Air-som-tillhor-Rafael.local>
2025-10-24 10:54:56 -0300

    setup

---
+++ src/test/java/com/guerlak/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guerlak.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
